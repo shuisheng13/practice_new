74626f07ebd79080ec653b912f809cf682cdccbc
yanghh <yanghh@autoai.com>
2020-04-24 10:24:57 +0800

    jvm提交

---
+++ jvm/src/main/java/com/yhh/pratice/jvm/HelloGc.java
@@ -0,0 +1,24 @@
+package com.yhh.pratice.jvm;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HelloGc {
+
+
+    /***
+     * 不断添加大对象  导致OOM
+     *  java.lang.OutOfMemoryError: Java heap space
+     * @param args
+     */
+    public static void main(String[] args) {
+        List list = new LinkedList();
+        for(;;){
+            byte[] bytes = new byte[1024*1024];
+            list.add(bytes);
+        }
+
+
+    }
+
+}
