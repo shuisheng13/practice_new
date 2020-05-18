ad9518b7ee79080ded7e18d21f5c0a78604eab7d
shuisheng13 <136158344@qq.com>
2020-05-18 23:10:42 +0800

    new commit

---
+++ jvm1/src/main/java/com/yhh/practice/jvm/runtimeDate/RuntimeVabile.java
@@ -0,0 +1,16 @@
+package com.yhh.practice.jvm.runtimeDate;
+
+public class RuntimeVabile {
+
+
+    public static void main(String[] args) {
+
+
+        int i = 8;
+
+        //i=i++;
+        ++i;
+        System.out.println(i);
+
+    }
+}
